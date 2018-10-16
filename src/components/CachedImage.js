28bbf677bc2345bea580241253493088dbad5a88
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-10-16 09:33:06 +0200

    Restructure, run linting

--- src/components/CachedImage.js
+++ src/components/CachedImage.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { View, Image, ActivityIndicator, Animated } from 'react-native';
+import React, { Component } from 'react'
+import { View, ActivityIndicator, Animated, Text } from 'react-native'
 import PropTypes from 'prop-types'
 import storage from '../helpers/AsyncLib'
 import { FileSystem } from 'expo'
-import { guid, getCacheDir, fetchB64, storeImageMeta} from '../components/CacheManager'
+import { guid, getCacheDir, fetchB64, storeImageMeta } from './CacheManager'
 
-export default class CachedImage extends Component {
+export default class Image extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -15,21 +15,22 @@ export default class CachedImage extends Component {
       opacity: new Animated.Value(0),
       loaderOpactiy: new Animated.Value(1)
     }
-    // This is an anti-pattern
+
     this.mounted = true
   }
 
   componentWillMount = () => {
     const { source } = this.props
-    source ? storage.get(source).then(res => res ? this.loadItem(res) : this.storeItem(source)) : this.setState({hasError: true})
+    source ? storage.get(source).then(res => res ? this.loadItem(res) : this.storeItem(source)) : this.setState({ hasError: true })
   }
 
-  componentWillUnmount = () => (this.mounted = false)
+  componentWillUnmount = () => { this.mounted = false }
 
   storeItem = async (source) => {
     const dir = await getCacheDir()
     const id = guid()
     let b46 = await fetchB64(source)
+
     FileSystem.writeAsStringAsync(dir.uri + id, b46).then((res) => {
       this.fadeOut(() => {
         if (this.mounted) {
@@ -42,9 +43,7 @@ export default class CachedImage extends Component {
           })
         }
       })
-    }).catch(error => {
-      console.error(error)
-    })
+    }).catch(this.props.onError)
   }
 
   loadItem = async (meta) => {
@@ -59,7 +58,7 @@ export default class CachedImage extends Component {
           })
         }
       })
-    }).catch(err => console.log(err))
+    }).catch(this.props.onError)
   }
 
   fadeIn = () => {
@@ -89,37 +88,37 @@ export default class CachedImage extends Component {
         style={[style, animatedStyles]}
       />
     ) : this.handleLoading(animatedLoaderStyles)
-    
   }
 
-  handleLoading = (loaderStyles) =>{
+  handleLoading = (loaderStyles) => {
     const { style, placeholderStyle, loaderSize, loaderColor, errorTextStyle } = this.props
 
     return this.state.hasError ? (
       <View style={[placeholderStyle, style]}>
         <Text style={[errorTextStyle]}>No image found</Text>
       </View>
-    ) : 
-    (
-      <Animated.View style={[placeholderStyle, style, loaderStyles]}>
-        <ActivityIndicator size={loaderSize} color={loaderColor} />
-      </Animated.View>
     )
-    
+      : (
+        <Animated.View style={[placeholderStyle, style, loaderStyles]}>
+          <ActivityIndicator size={loaderSize} color={loaderColor} />
+        </Animated.View>
+      )
   }
 }
 
-CachedImage.defaultProps = {
+Image.defaultProps = {
   source: '',
   style: {},
   placeholderStyle: {},
   errorTextStyle: {},
   loaderSize: 'small',
-  loaderColor: 'black'
+  loaderColor: 'black',
+  onError: (error) => console.error(error)
 }
 
-CachedImage.propTypes = {
+Image.propTypes = {
   source: PropTypes.string.isRequired,
   loaderSize: PropTypes.oneOf(['large', 'small']),
-  loaderColor: PropTypes.string
+  loaderColor: PropTypes.string,
+  onError: PropTypes.func
 }
--- src/helpers/AsyncLib.js
+++ src/helpers/AsyncLib.js
@@ -99,4 +99,4 @@ export default {
   update,
   remove,
   clear,
-  keys}
+  keys }
